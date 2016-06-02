5a3d476af7f7029acd130d97bf72445cacf84c65
svenyang <zhaoyang21cn@gmail.com>
2016-06-02 14:22:51 +0800

    更新TCAVIMDemo 注释，配置文档进行阅读

--- TCShow/TCAVIMDemo/TCAVIMDemo/AppDelegate.m
+++ TCShow/TCAVIMDemo/TCAVIMDemo/AppDelegate.m
@@ -23,17 +23,24 @@
 {
     if (!_liveHost)
     {
-
         // 用户修改默认主播角色， 主要是uid
         _liveHost = [[TCUser alloc] init];
+        
+        // 指定主播的ID
         _liveHost.uid = @"alexis";
+        
         _liveHost.name = @"alexis";
         _liveHost.icon = nil;
         
         _liveRoom = [[TCAVRoom alloc] init];
+        
+        // 指定直播间的房间号，房间号记得不要与demo一致，防止其他人也使用了相同的房间号，导致直播出现异常
         _liveRoom.liveAVRoomId = 3000000;
         _liveRoom.liveHost = _liveHost;
+        
+        // 指定直播间的聊天室ID，可以不与liveAVRoomId相同
         _liveRoom.liveIMChatRoomId = @"3000000";
+        
         _liveRoom.liveTitle = @"alexis Live";
     }
     self.window.rootViewController = [[TarBarController alloc] init];
